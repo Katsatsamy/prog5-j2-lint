0a47874852343af109bb463fb2b93087775a156d
Katsatsamy <velonyainaemilie@gmail.com>
2025-11-21 12:22:38 +0900

    feat: add checkstyle config and the class

---
+++ src/main/java/org/example/Wallet.java
@@ -0,0 +1,46 @@
+package org.example;
+
+public class Wallet {
+    private String color;
+    private String size;
+
+    public Wallet(String color, String size) {
+        this.color = color;
+        this.size = size;
+    }
+
+    public void addMoney(){
+        return;
+    }
+    public void getMoney(){
+        return;
+    }
+    public void open(){
+        return;
+    }
+    public void close(){
+        return;
+    }
+    public void isLost(){
+        return;
+    }
+    public void checkMoney(){
+        return;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+}
